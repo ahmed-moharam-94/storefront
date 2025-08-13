48ef4cba0d57567998f46ed68c658da0607a2066
ahmedezzat <ahmed.moharam.abuelyazeed@gmail.com>
2025-08-13 19:38:19 +0300

    Delete 0004 migration that accidently  Fix migration dependency by reverting to the previous migration

--- store/migrations/0005_auto_20210625_1631.py
+++ store/migrations/0005_auto_20210625_1631.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('store', '0004_auto_20210610_1442'),
+        ('store', '0003_add_slug_to_product'),
     ]
 
     operations = [
