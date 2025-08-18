deaa336e9ff249dc7b4b3fdcce676f7357cfeee1
ahmedezzat <ahmed.moharam.abuelyazeed@gmail.com>
2025-08-18 21:59:58 +0300

    test create collection when user is authenticated put not admin

--- store/tests/test_collections.py
+++ store/tests/test_collections.py
@@ -3,9 +3,9 @@ from rest_framework import status
 from rest_framework.test import APIClient
 import pytest
 
+
 @pytest.mark.django_db
 class TestCreateCollection:
-    @pytest.mark.skip
     def test_if_user_is_anonymous_return_401(self):
         # Arrange
 
@@ -15,3 +15,14 @@ class TestCreateCollection:
 
         # Assert
         assert response.status_code == status.HTTP_401_UNAUTHORIZED
+
+    def test_if_user_is_not_admin_return_401(self):
+        # Arrange
+
+        # Act
+        client = APIClient()
+        client.force_authenticate(user={})
+        response = client.post('/store/collections/', {'title': 'a'})
+
+        # Assert
+        assert response.status_code == status.HTTP_403_FORBIDDEN
