1b86dc226156d14fe5e8e77a582de4e387c86fda
ahmedezzat <ahmed.moharam.abuelyazeed@gmail.com>
2025-08-19 21:22:44 +0300

    Refactor collection creation tests to use new authenticate_user fixture

--- store/tests/conftest.py
+++ store/tests/conftest.py
@@ -1,6 +1,15 @@
 from rest_framework.test import APIClient
+from django.contrib.auth.models import User
 import pytest
 
 @pytest.fixture
 def api_client():
     return APIClient()
+
+@pytest.fixture
+def authenticate_user(api_client):
+    def do_authenticate(is_staff=False):
+        # you have to use the same api_client as the one that will make the request
+        # because this api_client that will be authenticated
+        return api_client.force_authenticate(user=User(is_staff=is_staff))
+    return do_authenticate
--- store/tests/test_collections.py
+++ store/tests/test_collections.py
@@ -1,45 +1,55 @@
 from http import client
+from venv import create
 from django.contrib.auth.models import User
 from rest_framework import status
 import pytest
+from rest_framework.test import APIClient
+
+
+@pytest.fixture
+def create_collection(api_client):
+    def do_create_collection(collection):
+        return api_client.post('/store/collections/', collection)
+    return do_create_collection
 
 
 @pytest.mark.django_db
 class TestCreateCollection:
-    def test_if_user_is_anonymous_return_401(self, api_client):
+    def test_if_user_is_anonymous_return_401(self,  create_collection):
         # Arrange
 
         # Act
-        response = api_client.post('/store/collections/', {'title': 'a'})
+        response = create_collection({'title': 'a'})
 
         # Assert
         assert response.status_code == status.HTTP_401_UNAUTHORIZED
 
-    def test_if_user_is_not_admin_return_401(self, api_client):
+    def test_if_user_is_not_admin_return_401(self, api_client, create_collection, authenticate_user):
         # Arrange
 
         # Act
-        api_client.force_authenticate(user={})
-        response = api_client.post('/store/collections/', {'title': 'a'})
+        authenticate_user()
+        response = create_collection({'title': 'a'})
 
         # Assert
         assert response.status_code == status.HTTP_403_FORBIDDEN
 
-    def test_if_user_is_admin_but_invalid_data_return_404(self, api_client):
+    def test_if_user_is_admin_but_invalid_data_return_404(self, api_client, create_collection, authenticate_user):
         # Arrange
         # Act
-        api_client.force_authenticate(user=User(is_staff=True))
-        response = api_client.post('/store/collections/', {'title': ''})
+        authenticate_user(is_staff=True)
+        response = create_collection({'title': ''})
         # Assert
         assert response.status_code == status.HTTP_400_BAD_REQUEST
         # when the title is invalid then the there will be error in title
         assert response.data['title'] is not None
 
-    def test_if_user_is_admin_but_valid_data_return_201(self, api_client):
+    def test_if_user_is_admin_but_valid_data_return_201(self, api_client, create_collection, authenticate_user, ):
         # Arrange
         # Act
-        api_client.force_authenticate(user=User(is_staff=True))
-        response = api_client.post('/store/collections/', {'title': 'a'})
+        authenticate_user(is_staff=True)
+        response = create_collection({'title': 'a'})
+
         # Assert
         assert response.status_code == status.HTTP_201_CREATED
         # when the data is valid check the id of the created object
