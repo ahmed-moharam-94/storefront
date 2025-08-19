32e4ea247f9b6e4100cf61c7b9fdc27f2945d633
ahmedezzat <ahmed.moharam.abuelyazeed@gmail.com>
2025-08-19 20:51:07 +0300

    Add pytest fixture for API client and refactor tests to use it

--- store/tests/test_collections.py
+++ store/tests/test_collections.py
@@ -1,50 +1,45 @@
 from http import client
 from django.contrib.auth.models import User
 from rest_framework import status
-from rest_framework.test import APIClient
 import pytest
 
 
 @pytest.mark.django_db
 class TestCreateCollection:
-    def test_if_user_is_anonymous_return_401(self):
+    def test_if_user_is_anonymous_return_401(self, api_client):
         # Arrange
 
         # Act
-        client = APIClient()
-        response = client.post('/store/collections/', {'title': 'a'})
+        response = api_client.post('/store/collections/', {'title': 'a'})
 
         # Assert
         assert response.status_code == status.HTTP_401_UNAUTHORIZED
 
-    def test_if_user_is_not_admin_return_401(self):
+    def test_if_user_is_not_admin_return_401(self, api_client):
         # Arrange
 
         # Act
-        client = APIClient()
-        client.force_authenticate(user={})
-        response = client.post('/store/collections/', {'title': 'a'})
+        api_client.force_authenticate(user={})
+        response = api_client.post('/store/collections/', {'title': 'a'})
 
         # Assert
         assert response.status_code == status.HTTP_403_FORBIDDEN
 
-    def test_if_user_is_admin_but_invalid_data_return_404(self):
+    def test_if_user_is_admin_but_invalid_data_return_404(self, api_client):
         # Arrange
         # Act
-        client = APIClient()
-        client.force_authenticate(user=User(is_staff=True))
-        response = client.post('/store/collections/', {'title': ''})
+        api_client.force_authenticate(user=User(is_staff=True))
+        response = api_client.post('/store/collections/', {'title': ''})
         # Assert
         assert response.status_code == status.HTTP_400_BAD_REQUEST
         # when the title is invalid then the there will be error in title
         assert response.data['title'] is not None
 
-    def test_if_user_is_admin_but_valid_data_return_201(self):
+    def test_if_user_is_admin_but_valid_data_return_201(self, api_client):
         # Arrange
         # Act
-        client = APIClient()
-        client.force_authenticate(user=User(is_staff=True))
-        response = client.post('/store/collections/', {'title': 'a'})
+        api_client.force_authenticate(user=User(is_staff=True))
+        response = api_client.post('/store/collections/', {'title': 'a'})
         # Assert
         assert response.status_code == status.HTTP_201_CREATED
         # when the data is valid check the id of the created object
