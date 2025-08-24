0544cd0a69b3c6c78c1a46bf9c494e15ccac3233
ahmed-moharam-94 <ahmed.moharam.abuelyazeed@gmail.com>
2025-08-24 11:37:33 +0300

    Add Locust user for browsing and update model formatting
    Introduced a Locust performance test script for browsing products, viewing product details, and adding items to cart. Also updated store/models.py to use consistent double quotes and improved formatting for readability.

--- locustfiles/browse_products.py
+++ locustfiles/browse_products.py
@@ -1,1 +1,43 @@
- 
+from random import randint
+from locust import HttpUser, TaskSet, task, between
+
+
+class WebstiteUser(HttpUser):
+    wait_time = between(1, 5)
+
+    @task(2)
+    def view_products(self):
+        # generate a collection_id randomly
+        collection_id = randint(2, 6)
+        # define a request
+        self.client.get(
+            f"/store/products/?collection_id={collection_id}",
+            name="/store/products/",
+        )
+
+    @task(4)
+    def view_product(self):
+        product_id = randint(1, 1000)
+        self.client.get(
+            f"/store/products/{product_id}",
+            name="/store/products/:id",
+        )
+
+    @task(1)
+    def add_to_cart(self):
+        # override on_start method to create a cart and get it's id
+        cart_id = self.cart_id
+        product_id = randint(1, 10)
+        self.client.post(
+            f"/store/carts/{cart_id}/items/",
+            name="/store/carts/items",
+            json={
+                "product_id": product_id,
+                "quantity": 1,
+            },
+        )
+
+    def on_start(self):
+        response = self.client.post("/store/carts/")
+        result = response.json()
+        self.cart_id = result["id"]
