c325b6be23f68604fbae860a4c2b4ec1380d68a3
ahmedezzat <ahmed.moharam.abuelyazeed@gmail.com>
2025-08-29 18:58:40 +0300

    set Redis on environment variables

--- storefront/settings/dev.py
+++ storefront/settings/dev.py
@@ -14,6 +14,19 @@ DATABASES = {
     }
 }
 
+CELERY_BROKER_URL = 'redis://localhost:6379/1'
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        "TIMEOUT": 10 * 60,
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
+
 if DEBUG:
       MIDDLEWARE += ['silk.middleware.SilkyMiddleware']
 
